a01f4dab4ff21f7e4a171b6d1affb226f2a2fe00
kalebprograms <136997786+kalebprograms@users.noreply.github.com>
2024-10-26 00:25:15 -0400

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -49,7 +49,7 @@ window.onload = function() {
 
     //load images
     birdImg = new Image();
-    birdImg.src = "./flappybird.png";
+    birdImg.src = "./umi.png.png";
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
