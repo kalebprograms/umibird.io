3d814f06807dcf44ebfebe4f51436c605cbfe410
kalebprograms <136997786+kalebprograms@users.noreply.github.com>
2024-10-26 00:34:31 -0400

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -6,8 +6,8 @@ let boardHeight = 640;
 let context;
 
 //bird
-let birdWidth = 34; //width/height ratio = 408/228 = 17/12
-let birdHeight = 24;
+let birdWidth = 50; //width/height ratio = 408/228 = 17/12
+let birdHeight = 50;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 let birdImg;
